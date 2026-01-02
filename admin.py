c67d5f84caa71a171e5175dba286eda045b11aea
M.FAHAD DAWOOD <muzamilfahad0@gmail.com>
2026-01-02 15:43:16 +0500

    added config.py

---
+++ admin.py
@@ -0,0 +1,36 @@
+from config import ADMIN_PASSWORD
+from movies import add_movie, view_movies, remove_movie, update_seats
+from bookings import view_bookings, total_revenue
+
+def admin_mode():
+    pwd = input("Enter admin password: ")
+    if pwd != ADMIN_PASSWORD:
+        print("❌ Wrong password!")
+        return
+
+    while True:
+        print("\n===== ADMIN MODE =====")
+        print("1. Add Movie")
+        print("2. View Movies")
+        print("3. Remove Movie")
+        print("4. Update Seats")
+        print("5. View Bookings")
+        print("6. Total Revenue")
+        print("7. Logout")
+
+        choice = input("Choice: ")
+
+        if choice == "1":
+            add_movie()
+        elif choice == "2":
+            view_movies()
+        elif choice == "3":
+            remove_movie()
+        elif choice == "4":
+            update_seats()
+        elif choice == "5":
+            view_bookings()
+        elif choice == "6":
+            total_revenue()
+        elif choice == "7":
+            break
---
+++ config.py
@@ -0,0 +1,4 @@
+ADMIN_PASSWORD = "admin123"
+
+MOVIE_FILE = "storage/movies.txt"
+BOOKING_FILE = "storage/bookings.txt"
